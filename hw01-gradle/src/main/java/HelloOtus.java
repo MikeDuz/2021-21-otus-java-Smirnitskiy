f78c9ea0cd8a3809c96184fe0a46dfbc04f566c1
MikeVS <smirnitskij_m_v@ntik.ru>
2022-07-14 20:31:08 +0300

    search solution of problem

--- hw01-gradle/src/main/java/HelloOtus.java
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -4,6 +4,7 @@ public class HelloOtus {
     public static void main(String[] args) {
         List<Integer> intList = Lists.newArrayList(1, 35, 8, 15, 25);
         intList.forEach(System.out::println);
+
     }
 
 }
