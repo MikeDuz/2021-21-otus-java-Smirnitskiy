06cd25d457f9df31939947180a0e936f85060891
MikeVS <smirnitskij_m_v@ntik.ru>
2022-07-14 20:17:09 +0300

    problem with dependency

--- hw01-gradle/src/main/java/HelloOtus.java
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -1,3 +1,9 @@
+import java.util.List;
+
 public class HelloOtus {
+    public static void main(String[] args) {
+        List<Integer> intList = Lists.newArrayList(1, 35, 8, 15, 25);
+        intList.forEach(System.out::println);
+    }
 
 }
